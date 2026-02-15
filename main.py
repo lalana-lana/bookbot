52c198d10f4fb5ca2a81c692c69fbf4b2a712fc7
lalana-lana <mollysgold2@gmail.com>
2026-02-15 14:56:20 -0800

    main.py can now read the contents of a file given a relative path

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 path_to_file = "books/frankenstein.txt"
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
-		file.contents = f.read(path_to_file)
-	return f.read
+		contents = f.read()
+	return contents
 def main():
 	text = get_book_text(path_to_file)
 	print(text)
