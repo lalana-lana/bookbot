bab629ef47c4cac39c118bfb5b8f9abb8924eea1
lalana-lana <mollysgold2@gmail.com>
2026-02-15 14:38:34 -0800

    initial setup for collab

---
+++ main.py
@@ -0,0 +1,10 @@
+#python3
+path_to_file = "books/frankenstein.txt"
+def get_book_text(path_to_file):
+	with open(path_to_file) as f:
+		file.contents = f.read(path_to_file)
+	return f.read
+def main():
+	text = get_book_text(path_to_file)
+	print(text)
+main()
