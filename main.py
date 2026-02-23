7b30f2172f1a88390cbaccc6c0b493299e444ec1
lalana-lana <mollysgold2@gmail.com>
2026-02-22 22:38:43 -0800

    letter count function added, modified main() after some realizations

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-from stats import word_count
-path_to_file = "books/frankenstein.txt"
+from stats import word_count, letter_count
+def main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_book_text(path_to_file)
+    words = word_count(text)
+    letters = letter_count(text)
+
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		contents = f.read()
 	return contents
+
+main()
--- stats.py
+++ stats.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-def word_count(get_book_text):
-	contents = get_book_text("books/frankenstein.txt")
+def word_count(text):
 	num_words = 0
-	for word in contents.split():
+	for word in text.split():
 		num_words += 1
 	print(f"Found {num_words} total words")
+
+def letter_count(text):
+    letters = {}
+    for (i, c) in enumerate(text.lower()):
+        if c not in letters:
+            letters[c] = 1
+        if c in letters:
+            letters[c] += 1
+    print(letters)
